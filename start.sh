3d586943f2a0f729c2cb7f999cdf51e71a973586
sublani <sgjgonza@gmail.com>
2021-10-15 15:32:26 +0000

    update

--- start.sh
+++ start.sh
@@ -6,13 +6,13 @@ docker system prune --all -f
 
 docker build -t api:0.1.0 . 
 
-
 sleep 15
 
 # POR SEPARADO
 docker-compose -f docker/docker-compose.db.yml up -d
 
-sleep 20
+sleep 20 
+
 docker-compose -f docker/docker-compose.app.yml up -d
 
 # JUNTO
